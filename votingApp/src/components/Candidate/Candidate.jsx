8e77908a1dd12e55246b2393e62a20fc39ab34c8
layan10 <layanhaddad5@gmail.com>
2024-11-17 20:03:45 +0200

    updated voting mechanism

--- votingApp/src/components/Candidate/Candidate.jsx
+++ votingApp/src/components/Candidate/Candidate.jsx
@@ -3,54 +3,67 @@ import { useState } from 'react';
 import PropTypes from 'prop-types';
 import ChangeMyVote from '../ChangeMyVote/ChangeMyVote';
 
-const Candidate = ({ candidate }) => {
-    const [votes, setVotes] = useState(candidate.votes);
-    const [btnTitle, setBtnTitle] = useState('Vote');
-    const [showChangeVoteOptions, setShowChangeVoteOptions] = useState(false);
+const Candidate = ({ candidate, votedCandidateId, onVote, onCancelVote }) => {
+  const [votes, setVotes] = useState(candidate.votes);
+  const [btnTitle, setBtnTitle] = useState('Vote');
+  const [showChangeVoteOptions, setShowChangeVoteOptions] = useState(false);
 
-    const handleVote = () => {
-        setVotes(votes + 1);
-        setBtnTitle('Change My Vote');
-    };
+  const handleVote = () => {
+    setVotes(votes + 1);
+    setBtnTitle('Change My Vote');
+    onVote(candidate.id); 
+  };
 
-    const handleChangeVoteClick = () => {
-        setShowChangeVoteOptions(true);
-    };
+  const handleChangeVoteClick = () => {
+    setShowChangeVoteOptions(true);
+  };
 
-    const handleConfirmChange = () => {
-        setVotes(votes - 1);
-        setBtnTitle('Vote');
-        setShowChangeVoteOptions(false);
-    };
+  const handleConfirmChange = () => {
+    setVotes(votes - 1);
+    setBtnTitle('Vote');
+    setShowChangeVoteOptions(false);
+    onCancelVote(); 
+  };
 
-    const handleCancelChange = () => {
-        setShowChangeVoteOptions(false);
-    };
+  const handleCancelChange = () => {
+    setShowChangeVoteOptions(false);
+  };
 
-    return (
-        <div className="candidate">
-            <img src={candidate.image} alt={candidate.name} />
-            <h3 className="candidate-name">{candidate.name}</h3>
-            {!showChangeVoteOptions ? (
-                <button onClick={btnTitle === 'Vote' ? handleVote : handleChangeVoteClick}>
-                    {btnTitle}
-                </button>
-            ) : (
-                <ChangeMyVote onConfirm={handleConfirmChange} onCancel={handleCancelChange} />
-            )}
-            <p>
-                Votes: <span>{votes}</span>
-            </p>
-        </div>
-    );
+  const shouldShowVoteButton =
+    votedCandidateId === null || votedCandidateId === candidate.id;
+
+  return (
+    <div className="candidate">
+      <img src={candidate.image} alt={candidate.name} />
+      <h3 className="candidate-name">{candidate.name}</h3>
+      {!showChangeVoteOptions ? (
+        shouldShowVoteButton && (
+          <button
+            onClick={btnTitle === 'Vote' ? handleVote : handleChangeVoteClick}
+          >
+            {btnTitle}
+          </button>
+        )
+      ) : (
+        <ChangeMyVote onConfirm={handleConfirmChange} onCancel={handleCancelChange} />
+      )}
+      <p>
+        Votes: <span>{votes}</span>
+      </p>
+    </div>
+  );
 };
 
 Candidate.propTypes = {
-    candidate: PropTypes.shape({
-        votes: PropTypes.number.isRequired,
-        image: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-    }).isRequired,
+  candidate: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    votes: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+  votedCandidateId: PropTypes.number,
+  onVote: PropTypes.func.isRequired,
+  onCancelVote: PropTypes.func.isRequired,
 };
 
 export default Candidate;
--- votingApp/src/pages/VotingPage/VotingPage.jsx
+++ votingApp/src/pages/VotingPage/VotingPage.jsx
@@ -1,11 +1,20 @@
 import Candidate from "../../components/Candidate/Candidate";
 import { CandidatesContext } from "../../context/CandidatesContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "./VotingPage.css";
 import PropTypes from 'prop-types';
 
-const VotingPage = ({logo}) => {
+const VotingPage = ({ logo }) => {
   const { candidates } = useContext(CandidatesContext);
+  const [votedCandidateId, setVotedCandidateId] = useState(null);
+
+  const handleVote = (candidateId) => {
+    setVotedCandidateId(candidateId);
+  };
+
+  const handleCancelVote = () => {
+    setVotedCandidateId(null);
+  };
 
   return (
     <div className="voting-page">
@@ -17,8 +26,14 @@ const VotingPage = ({logo}) => {
       </nav>
       <h1>Voting Page</h1>
       <div className="candidates">
-         {candidates.map(candidate => (
-          <Candidate key={candidate.id} candidate={candidate} />
+        {candidates.map(candidate => (
+          <Candidate
+            key={candidate.id}
+            candidate={candidate}
+            votedCandidateId={votedCandidateId}
+            onVote={handleVote}
+            onCancelVote={handleCancelVote}
+          />
         ))}
       </div>
     </div>
